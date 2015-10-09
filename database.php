2c3cac02b1d6d6ea39f6fb9f081294e05e094301
devrimgl <devrimgunyel@gmail.com>
2015-10-09 11:00:18 +0100

    Adding my own DB credentials

--- database.php
+++ database.php
@@ -4,9 +4,9 @@ class Db {
     
     protected $con;
     private $host = "us-cdbr-azure-west-c.cloudapp.net";
-    private $user = "bcfde92a608269";
-    private $pwd = "815873fb";
-    private $db = "acsm_8cd991cf173de96";
+    private $user = "beb85ac56ef646";
+    private $pwd = "6cfd854a";
+    private $db = "TecLog1450232DB";
     
     //Creates a PDO conection & sets error mode to exceptions
     public function __construct(){
